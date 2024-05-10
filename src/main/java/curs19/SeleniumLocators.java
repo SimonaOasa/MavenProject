32873be772e4d4a57728c4bccb5fe3bf98f97aaa
simona oasa <simona.oasa@outlook.com>
2024-05-10 16:55:44 +0300

    curs20 jumatate

--- src/main/java/curs19/SeleniumLocators.java
+++ src/main/java/curs19/SeleniumLocators.java
@@ -5,6 +5,7 @@ import static org.testng.Assert.assertEquals;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import selenium.utils.BaseTest;
@@ -50,7 +51,7 @@ public class SeleniumLocators extends BaseTest {
 		
 		driver.findElement(By.linkText("BOOKS")).click(); 
 		driver.getCurrentUrl();//citeste url curent la care se afla browser ul
-		assertEquals(driver.getCurrentUrl(),"https://keybooks.ro/");
+		assertEquals(driver.getCurrentUrl(),"https://keybooks.ro/shop/");
 	}
 	
 	@Test(priority = 3)
@@ -68,8 +69,46 @@ public class SeleniumLocators extends BaseTest {
 	@Test(priority = 4)
 	public void classNameLocator() {
 		
-		WebElement price = driver.findElement(By.className("price"));
+		WebElement price = driver.findElement(By.className("price")); 
 		System.out.println(price.getText());
-		assertTrue(price.getText().contains("18.50"));
+		assertTrue(price.getText().contains("18.49"));
 	}
+	
+	@Test(priority = 5)
+	public void idLocator() {
+		Actions action = new Actions(driver);
+		action.scrollByAmount(0, 500).perform();
+		WebElement reviewTab = driver.findElement(By.id("tab-title-reviews"));
+		reviewTab.click();
+		WebElement commentBox  = driver.findElement(By.id("comment"));
+		assertTrue(commentBox.isDisplayed());
+		
+	}
+	
+	@Test(priority = 6)
+	public void nameLocator() throws InterruptedException {
+		
+		WebElement commentBox  = driver.findElement(By.name("comment"));
+		commentBox.sendKeys("Super mesaj!");
+		Thread.sleep(3000);
+		commentBox.clear();//sterge valoearea prezenta intr un input sau text area field
+		commentBox.sendKeys("Alt Super mesaj!");
+		
+	}
+	
+	@Test(priority = 7)
+	public void cssSelectorLocator() {
+		//input[id='author']
+		driver.findElement(By.cssSelector("input[id='author']")).sendKeys("John Doe");
+		
+	}
+	
+	@Test(priority = 8)
+	public void xpathLocator() {
+		
+		////input[@type='email']
+		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("test@test.com");
+		
+	}
+	
 }
